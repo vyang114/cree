e20305c90c3bb94bab4b4ace411dd98889140d69
Vera <veray@sfu.ca>
2023-11-02 05:20:22 -0700

    Element4

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -8,9 +8,9 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
     <Link className="navbar-brand" to="/">Cree Revitalization</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
       <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
